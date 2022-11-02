8f76d70e5a7836290ca1991d465807e593007167
Seregadixi <seregadixi@mail.ru>
2022-11-02 11:14:31 +0300

    z1

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+$json = file_get_contents('./phone-codes.json');
+
+
+$json = json_decode($json, true);
+foreach($json as $img) {
+
+}
+
+
---
+++ js/app.js
@@ -0,0 +1,20 @@
+$("body").on("submit", ".form", function (e) {
+    e.preventDefault();
+    var _phone = $(this).find("#codtel").val();
+    let str = String(_phone);
+    console.log(str.length);
+        if ($('#codtel').val()[0] != '+' && $('#codtel').val() != '') {
+
+
+            var b = '+';
+            b = b.concat(_phone);
+            console.log(b);
+        }
+
+
+
+
+
+    return false;
+
+});
